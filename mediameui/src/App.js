7c663ed7495d2227ef95e4dc64e0e34610aa78fd
khalid2-dev <k.abushaban2@gmail.com>
2024-07-08 19:40:41 -0400

    adding new code

--- mediameui/src/App.js
+++ mediameui/src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+      axios.get('http://localhost:8080/welcome')
+          .then(response => {
+              setMessage(response.data);
+          })
+          .catch(error => {
+              console.error('There was an error fetching the data!', error);
+          });
+  }, []);
+
+  return (
+      <div className="App">
+          <header className="App-header">
+              <p>{message}</p>
+          </header>
+      </div>
+  );
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +41,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
 }
 
 export default App;
